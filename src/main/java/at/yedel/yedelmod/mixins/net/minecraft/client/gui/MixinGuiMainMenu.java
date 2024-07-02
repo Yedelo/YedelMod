e05b1b2cfa7a3cc42763257471f77d1c8e8e82fc
BuildTools <unconfigured@null.spigotmc.org>
2024-07-02 18:13:21 -0400

    Some adjustments to MixinGuiMainMenu

--- src/main/java/at/yedel/yedelmod/mixins/net/minecraft/client/gui/MixinGuiMainMenu.java
+++ src/main/java/at/yedel/yedelmod/mixins/net/minecraft/client/gui/MixinGuiMainMenu.java
@@ -22,27 +22,23 @@ import static at.yedel.yedelmod.YedelMod.minecraft;
 @Mixin(GuiMainMenu.class)
 public abstract class MixinGuiMainMenu extends GuiScreen {
     @Unique
-    private final GuiButton yedelmod$favoriteServerButton = new GuiButton(1539, 5, 5, 125, 20, "Join Favorite Server");
+    private GuiButton yedelmod$favoriteServerButton;
 
     @Inject(method = "initGui", at = @At("TAIL"))
     private void yedelmod$addGuiButtons(CallbackInfo ci) {
-        if (YedelConfig.getInstance().buttonFavoriteServer) this.buttonList.add(yedelmod$favoriteServerButton);
+        if (YedelConfig.getInstance().buttonFavoriteServer) {
+            this.buttonList.add(yedelmod$favoriteServerButton = new GuiButton(1600, 5, 5, 125, 20, "Join Favorite Server"));
+        }
     }
 
     @Inject(method = "actionPerformed", at = @At("TAIL"))
     private void yedelmod$registerButtonPress(GuiButton button, CallbackInfo ci) {
-        if (button.id == 1539) {
+        if (button == yedelmod$favoriteServerButton) {
             minecraft.addScheduledTask(() -> {
                 FMLClientHandler.instance().connectToServer(
                         new GuiMultiplayer(new GuiMainMenu()), new ServerData("Favorite Server", YedelConfig.getInstance().favoriteServer, false)
                 );
             });
         }
-    }
-
-    @Inject(method = "drawScreen", at = @At("TAIL"))
-    private void yedelmod$drawButton(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
-        if (YedelConfig.getInstance().buttonFavoriteServer)
-            yedelmod$favoriteServerButton.drawButton(minecraft, mouseX, mouseY);
     }
 }
