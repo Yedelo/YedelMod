810ffe421154259c128d6832b871da0f59bebe35
BuildTools <unconfigured@null.spigotmc.org>
2024-11-05 14:53:23 -0500

    Removed empty arg errors (since oneconfig handles them), changed -update and -ping commands to sub command groups

--- src/main/java/at/yedel/yedelmod/features/YedelCommand.java
+++ src/main/java/at/yedel/yedelmod/features/YedelCommand.java
@@ -19,6 +19,7 @@ import cc.polyfrost.oneconfig.utils.commands.annotations.Command;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Greedy;
 import cc.polyfrost.oneconfig.utils.commands.annotations.Main;
 import cc.polyfrost.oneconfig.utils.commands.annotations.SubCommand;
+import cc.polyfrost.oneconfig.utils.commands.annotations.SubCommandGroup;
 import org.apache.commons.lang3.StringUtils;
 import org.lwjgl.opengl.Display;
 
@@ -64,7 +65,7 @@ public class YedelCommand {
 		Chat.display(Messages.formattingGuideMessage);
 	}
 
-	@SubCommand(description = "Sends an illegal chat character, which disconnects you on most servers and sends you to limbo-like areas on some. No longer works on Hypixel, use /limbo instead.", aliases = {"li"})
+	@SubCommand(description = "Sends an illegal chat character, which disconnects you on most servers and sends you to limbo-like areas on some. No longer works on Hypixel, use /limbo instead.", aliases = "li")
 	private void limbo() {
 		Chat.say("§");
 	}
@@ -79,12 +80,45 @@ public class YedelCommand {
 		Functions.displayScreen(new MoveHudGui(minecraft.currentScreen));
 	}
 
-	@SubCommand(description = "Shows your ping to the server in chat, using several methods. Without an argument, uses the default method which can be customized.")
-	private void ping(String method) {
-		PingSender.getInstance().processPingCommand(method);
+	@SubCommandGroup(value = "ping")
+	private static class Ping {
+		@Main(description = "Shows your ping to the server in chat with the method specified in the config.")
+		private void main() {
+			PingSender.getInstance().defaultMethodPing();
+		}
+
+		@SubCommand(description = "Does /ping command. Works on very few servers.", aliases = "p")
+		private void ping() {
+			PingSender.getInstance().pingPing();
+		}
+
+		@SubCommand(description = "Enters a random command and waits for the unknown command response. Works on almost all servers.", aliases = "c")
+		private void command() {
+			PingSender.getInstance().commandPing();
+		}
+
+		@SubCommand(description = "Sends a tab completion packet and waits for the response. Works on all servers.", aliases = "t")
+		private void tab() {
+			PingSender.getInstance().tabPing();
+		}
+
+		@SubCommand(description = "Sends a statistics packet and waits for the response. Works on all servers.", aliases = "s")
+		private void stats() {
+			PingSender.getInstance().statsPing();
+		}
+
+		@SubCommand(description = "Gets the ping displayed previously on the server list. Doesn't work on singleplayer and if you used Direct Connect.", aliases = "l")
+		private void list() {
+			PingSender.getInstance().serverListPing();
+		}
+
+		@SubCommand(description = "Uses the Hypixel ping packet and waits for the response. Only works on hypixel.", aliases = "h")
+		private void hypixel() {
+			PingSender.getInstance().hypixelPing();
+		}
 	}
 
-	@SubCommand(description = "Shows your total playtime (while playing on servers) in hours and minutes.", aliases = {"pt"})
+	@SubCommand(description = "Shows your total playtime (while playing on servers) in hours and minutes.", aliases = "pt")
 	private void playtime() {
 		int minutes = YedelConfig.getInstance().playtimeMinutes;
 		Chat.logoDisplay("&ePlaytime: &6" + minutes / 60 + " hours &eand &6" + minutes % 60 + " minutes");
@@ -92,61 +126,48 @@ public class YedelCommand {
 
 	@SubCommand(description = "Sets your nick for Bounty Hunting to not select yourself as the target.")
 	private void setnick(String nick) {
-		if (isArgEmpty(nick)) {
-			Chat.display(Messages.enterValidNick);
-		}
-		else {
-			Chat.display("&6&l[BountyHunting] §eSet nick to " + nick + "§e!");
-			YedelConfig.getInstance().nick = nick;
-			YedelConfig.getInstance().save();
-		}
+		Chat.display("&6&l[BountyHunting] §eSet nick to " + nick + "§e!");
+		YedelConfig.getInstance().nick = nick;
+		YedelConfig.getInstance().save();
 	}
 
 	@SubCommand(description = "Sets the display text, supporting color codes with ampersands (&).")
 	private void settext(@Greedy String text) {
-		if (isArgEmpty(text)) {
-			Chat.display(Messages.enterValidText);
-		}
-		else {
-			YedelConfig.getInstance().displayedText = text;
-			YedelConfig.getInstance().save();
-			Chat.logoDisplay("&eSet displayed text to \"&r" + text + "&e\"!");
-		}
+		YedelConfig.getInstance().displayedText = text;
+		YedelConfig.getInstance().save();
+		Chat.logoDisplay("&eSet displayed text to \"&r" + text + "&e\"!");
 	}
 
 	@SubCommand(description = "Sets the title of the game window.")
 	private void settitle(@Greedy String text) {
-		if (isArgEmpty(text)) {
-			Chat.display(Messages.enterValidTitle);
-			return;
-		}
 		Chat.logoDisplay("&eSet display title to \"&f" + text + "&e\"!");
 		Display.setTitle(text);
 	}
 
-	@SubCommand(description = "Simulates a chat message, also supports color codes with ampersands (&).", aliases = {"simc"})
+	@SubCommand(description = "Simulates a chat message, also supports color codes with ampersands (&).", aliases = "simc")
 	private void simulatechat(@Greedy String text) {
 		Chat.display(text);
 	}
 
-	@SubCommand(description = "Checks for mod updates. Without an argument, uses the default one (modrinth). Possible platforms are \"modrinth\" or \"github\".")
-	private void update(String method) {
-		if (isArgEmpty(method)) {
+	@SubCommandGroup(value = "update")
+	private static class Update {
+		@Main(description = "Checks for mod updates with the method specified in the config.")
+		private void main() {
 			UpdateManager.getInstance().checkForUpdates(YedelConfig.getInstance().getUpdateSource(), FeedbackMethod.CHAT);
-			return;
 		}
-		if (method.equals("modrinth")) {
+
+		@SubCommand(description = "Checks for mod updates on Modrinth.")
+		private void modrinth() {
 			UpdateManager.getInstance().checkForUpdates(UpdateSource.MODRINTH, FeedbackMethod.CHAT);
 		}
-		else if (method.equals("github")) {
+
+		@SubCommand(description = "Checks for mod updates on GitHub.")
+		private void github() {
 			UpdateManager.getInstance().checkForUpdates(UpdateSource.GITHUB, FeedbackMethod.CHAT);
-		}
-		else {
-			UpdateManager.getInstance().checkForUpdates(YedelConfig.getInstance().getUpdateSource(), FeedbackMethod.CHAT);
 		}
 	}
 
-	@SubCommand(description = "Shows messages from me about the mod. These can be anything from tips to bug notices.", aliases = {"message"})
+	@SubCommand(description = "Shows messages from me about the mod. These can be anything from tips to bug notices.", aliases = "message")
 	private void yedelmessage() {
 		new Thread(() -> {
 			try {
