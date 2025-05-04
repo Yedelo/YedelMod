698f6d2f46b12b3a3a6906ba3ab86cb1b211711b
BuildTools <unconfigured@null.spigotmc.org>
2025-05-04 11:43:42 -0400

    Update dummy Notification class to crash if it were to be used

--- src/ducks/java/cc/polyfrost/oneconfig/internal/utils/Notification.java
+++ src/ducks/java/cc/polyfrost/oneconfig/internal/utils/Notification.java
@@ -11,5 +11,7 @@ import java.util.concurrent.Callable;
 public class Notification {
     public Notification(String title, String message, Icon icon, float duration, Callable<Float> progressbar, Runnable action) {}
 
-    public boolean isFinished() {return true;}
+    public boolean isFinished() {
+        throw new IllegalStateException("Dummy code called!");
+    }
 }
