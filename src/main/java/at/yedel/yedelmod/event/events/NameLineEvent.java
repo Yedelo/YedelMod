89187e1e54e1ab5bf46987fe523edd19de76c528
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-06-06 10:43:03 -0400

    Make NameLineEvent a forge event to make testing easier

--- src/main/java/at/yedel/yedelmod/event/events/NameLineEvent.java
+++ src/main/java/at/yedel/yedelmod/event/events/NameLineEvent.java
@@ -3,13 +3,14 @@ package at.yedel.yedelmod.event.events;
 
 
 import at.yedel.yedelmod.utils.NameLine;
+import net.minecraftforge.fml.common.eventhandler.Event;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 
-public class NameLineEvent {
+public class NameLineEvent extends Event {
     private final List<NameLine> nameLines = new ArrayList<>();
 
     public List<NameLine> getNameLines() {
@@ -17,7 +18,7 @@ public class NameLineEvent {
     }
 
     public NameLine newNameLine(String text) {
-        NameLine nameLine = new NameLine(text);
+        NameLine nameLine = new NameLine().text(text);
         nameLines.add(nameLine);
         return nameLine;
     }
