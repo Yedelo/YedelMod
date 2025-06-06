f4025ca8edd753e6deae8ff7eae9b58ff28bbe9e
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-06-06 10:32:47 -0400

    Add unimplemented NameLine API

---
+++ src/main/java/at/yedel/yedelmod/event/events/NameLineEvent.java
@@ -0,0 +1,24 @@
+package at.yedel.yedelmod.event.events;
+
+
+
+import at.yedel.yedelmod.utils.NameLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class NameLineEvent {
+    private final List<NameLine> nameLines = new ArrayList<>();
+
+    public List<NameLine> getNameLines() {
+        return nameLines;
+    }
+
+    public NameLine newNameLine(String text) {
+        NameLine nameLine = new NameLine(text);
+        nameLines.add(nameLine);
+        return nameLine;
+    }
+}
---
+++ src/main/java/at/yedel/yedelmod/utils/NameLine.java
@@ -0,0 +1,87 @@
+package at.yedel.yedelmod.utils;
+
+
+
+import cc.polyfrost.oneconfig.libs.universal.wrappers.UPlayer;
+import com.google.common.base.Strings;
+import net.minecraft.entity.player.EntityPlayer;
+
+import java.util.function.Predicate;
+
+
+
+public class NameLine {
+    private String text;
+    private boolean showWhileSneaking = true;
+    private boolean showWhileInvisible = false;
+    private int maxDistance = 64;
+    private final Predicate<EntityPlayer> DEFAULT_SHOW_PREDICATE = (player) -> {
+        if (Strings.isNullOrEmpty(text)) {
+            return false;
+        }
+        if (!showWhileSneaking && player.isSneaking()) {
+            return false;
+        }
+        if (!showWhileInvisible && player.isInvisible()) {
+            return false;
+        }
+        if (player.getDistanceSqToEntity(UPlayer.getPlayer()) > maxDistance) {
+            return false;
+        }
+        return true;
+    };
+    private Predicate<EntityPlayer> shouldShow = DEFAULT_SHOW_PREDICATE;
+
+    public NameLine text(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public NameLine empty() {
+        return text("");
+    }
+
+    public NameLine showWhileSneaking(boolean showWhileSneaking) {
+        this.showWhileSneaking = showWhileSneaking;
+        return this;
+    }
+
+    public NameLine showWhileSneaking() {
+        return showWhileSneaking(true);
+    }
+
+    public NameLine hideWhileSneaking() {
+        return showWhileSneaking(false);
+    }
+
+    public NameLine showWhileInvisible(boolean showWhileInvisible) {
+        this.showWhileInvisible = showWhileInvisible;
+        return this;
+    }
+
+    public NameLine showWhileInvisible() {
+        return showWhileInvisible(true);
+    }
+
+    public NameLine hideWhileInvisible() {
+        return showWhileInvisible(false);
+    }
+
+    public NameLine maxDistance(int maxDistance) {
+        this.maxDistance = maxDistance;
+        return this;
+    }
+
+    public NameLine shouldShow(Predicate<EntityPlayer> shouldShow) {
+        this.shouldShow = shouldShow;
+        return this;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean shouldShow(EntityPlayer player) {
+        return shouldShow.test(player);
+    }
+}
