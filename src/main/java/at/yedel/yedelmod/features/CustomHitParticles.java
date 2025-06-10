a64f0ae6928e2818abed8a051e60a9f2928515f5
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-06-10 13:39:11 -0400

    Fix ITEM_CRACK, BLOCK_CRACK and BLOCK_DUST particles crashing by adding formal parameters to spawnParticle

--- src/main/java/at/yedel/yedelmod/features/CustomHitParticles.java
+++ src/main/java/at/yedel/yedelmod/features/CustomHitParticles.java
@@ -1,11 +1,16 @@
 package at.yedel.yedelmod.features;
 
 
+
 import at.yedel.yedelmod.config.YedelConfig;
 import at.yedel.yedelmod.utils.NumberUtils;
 import cc.polyfrost.oneconfig.libs.universal.UMinecraft;
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -33,7 +38,30 @@ public class CustomHitParticles {
 			}
 			int particleId =
 				YedelConfig.getInstance().randomParticleType ? (int) NumberUtils.randomRange(0, 41) : YedelConfig.getInstance().customParticleType;
-			UMinecraft.getWorld().spawnParticle(EnumParticleTypes.getParticleFromId(particleId), true, entity.posX, entity.posY + YedelConfig.getInstance().particleYOffset, entity.posZ, 0, 0, 0, 0, 0);
+			EnumParticleTypes particle = EnumParticleTypes.getParticleFromId(particleId);
+			int parameters;
+			/*
+			    Some particles take required parameters.
+			    ITEM_CRACK:
+			        0: item id
+			        1: item metadata (unrequired)
+			    BLOCK_CRACK:
+			        0: special block number (holding both block id and state)
+			    BLOCK_DUST:
+			        0: special block number (holding both block id and state)
+			 */
+			switch (particle) {
+				case ITEM_CRACK:
+					parameters = Item.getIdFromItem(Items.redstone);
+					break;
+				case BLOCK_CRACK:
+				case BLOCK_DUST:
+					parameters = Block.getStateId(Blocks.redstone_ore.getDefaultState());
+					break;
+				default:
+					parameters = 0;
+			}
+			UMinecraft.getWorld().spawnParticle(particle, true, entity.posX, entity.posY + YedelConfig.getInstance().particleYOffset, entity.posZ, 0, 0, 0, parameters);
 		}
 	}
 }
