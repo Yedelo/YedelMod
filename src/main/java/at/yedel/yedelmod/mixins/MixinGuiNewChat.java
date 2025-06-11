e7c5fcbc636d713661f7a6f3ad26be01e429c600
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-06-11 13:58:36 -0400

    Replace lan chat message mixin with integrated server mixin

--- src/main/java/at/yedel/yedelmod/mixins/MixinGuiNewChat.java
+++ src/main/java/at/yedel/yedelmod/mixins/MixinGuiNewChat.java
@@ -3,15 +3,10 @@ package at.yedel.yedelmod.mixins;
 
 
 import at.yedel.yedelmod.config.YedelConfig;
-import cc.polyfrost.oneconfig.libs.universal.wrappers.message.UTextComponent;
 import net.minecraft.client.gui.GuiNewChat;
-import net.minecraft.util.IChatComponent;
-import org.lwjgl.opengl.Display;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -19,16 +14,6 @@ import java.util.List;
 
 @Mixin(GuiNewChat.class)
 public abstract class MixinGuiNewChat {
-    // Lan message is printed to the chat GUI and skips ClientChatReceivedEvent. Listening to logs may be possible here
-    @Inject(method = "printChatMessage", at = @At("HEAD"))
-    private void yedelmod$onLanHost(IChatComponent chatComponent, CallbackInfo ci) {
-        if (chatComponent.getUnformattedText().startsWith("Local game hosted on port")) {
-            String port = UTextComponent.Companion.stripFormatting(chatComponent.getFormattedText().substring(26));
-            if (YedelConfig.getInstance().enabled && YedelConfig.getInstance().changeWindowTitle)
-                Display.setTitle("Minecraft 1.8.9 - LAN Singleplayer - " + port);
-        }
-    }
-
     @Redirect(
         method = "clearChatMessages",
         at = @At(value = "INVOKE", target = "Ljava/util/List;clear()V", ordinal = 2)
---
+++ src/main/java/at/yedel/yedelmod/mixins/MixinIntegratedServer.java
@@ -0,0 +1,23 @@
+package at.yedel.yedelmod.mixins;
+
+
+
+import at.yedel.yedelmod.config.YedelConfig;
+import net.minecraft.server.integrated.IntegratedServer;
+import org.lwjgl.opengl.Display;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+
+
+@Mixin(IntegratedServer.class)
+public abstract class MixinIntegratedServer {
+    @ModifyArg(method = "shareToLAN", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetworkSystem;addLanEndpoint(Ljava/net/InetAddress;I)V"), index = 1)
+    private int yedelmod$setLanPortTitle(int port) {
+        if (YedelConfig.getInstance().enabled && YedelConfig.getInstance().changeWindowTitle) {
+            Display.setTitle("Minecraft 1.8.9 - LAN Singleplayer - " + port);
+        }
+        return port;
+    }
+}
