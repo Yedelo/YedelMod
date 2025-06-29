1ade78539dc9846b608a1568a9c153ed4473186c
Yedelo <158670400+Yedelo@users.noreply.github.com>
2025-06-29 18:41:06 -0400

    Replace oneconfig helper with manual oneconfig dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,14 +25,10 @@ plugins {
     )) id("dev.deftu.gradle.tools.$tool")
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-}
-
 toolkitLoomHelper {
     disableRunConfigs(GameSide.SERVER)
     val _polyMixinVersion = polyMixinVersion
+    /*
     useOneConfig {
         version = oneconfigVersion
         loaderVersion = oneconfigLoaderVersion
@@ -51,6 +47,8 @@ toolkitLoomHelper {
         +"utils"
         +"internal"
     }
+     */
+    useTweaker("org.polyfrost.oneconfig.loader.stage0.LaunchWrapperTweaker")
 
     if (mcData.isLegacyForge) {
         useForgeMixin(modData.id)
@@ -64,6 +62,19 @@ toolkitLoomHelper {
         println("Using resource pack directory $resourcePackDir from environment variable minecraft.resourcePackDir")
         useArgument("--resourcePackDir", resourcePackDir, GameSide.BOTH)
     }
+}
+
+dependencies {
+    implementation("org.polyfrost.oneconfig:stage0:$oneconfigLoaderVersion:all")
+    compileOnly("org.polyfrost.oneconfig:stage0:$polyMixinVersion")
+
+    for (module in arrayOf("commands", "config", "config-impl", "events", "hud", "ui", "utils", "internal")) {
+        compileOnly("org.polyfrost.oneconfig:$module:$oneconfigVersion")
+    }
+    compileOnly("org.polyfrost.oneconfig:1.8.9-forge:$oneconfigVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
 tasks {
