781a7daeac89877121a8420d8d3f7857e417a4ab
Yedelo <158670400+yedelo@users.noreply.github.com>
2026-08-29 05:20:22 -0400

    Remove PingException subclasses

--- src/main/java/at/yedel/yedelmod/features/ping/PingException.java
+++ src/main/java/at/yedel/yedelmod/features/ping/PingException.java
@@ -6,16 +6,4 @@ public class PingException extends RuntimeException {
     public PingException(String message) {
         super(message);
     }
-
-    public static class Send extends PingException {
-        public Send(String message) {
-            super(message);
-        }
-    }
-
-    public static class Receive extends PingException {
-        public Receive(String message) {
-            super(message);
-        }
-    }
 }
